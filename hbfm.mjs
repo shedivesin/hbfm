caf2d6885213b9519f70bcef117e31b46dd7241b
shedivesin <>
2024-02-21 15:25:16 -0600

    More user-friendly.

--- hbfm.mjs
+++ hbfm.mjs
@@ -13,7 +13,7 @@ function brainfuck(code, input) {
         data[p] = (data[p] + 1) & 255;
         break;
       case ",":
-        data[p] = input.charCodeAt(i++);
+        data[p] = input.charCodeAt(i++) & 255;
         break;
       case "-":
         if(data[p] === 0) { portable = false; }
@@ -23,19 +23,19 @@ function brainfuck(code, input) {
         output += String.fromCharCode(data[p]);
         break;
       case "<":
-        if(p === 0) { portable = false; data.unshift(0); ++p; }
+        if(p === 0) { portable = false; data.unshift(0); p++; }
         --p;
         break;
       case ">":
         if(p === 29999) { portable = false; }
         if(p === data.length - 1) { data.push(0); }
-        ++p;
+        p++;
         break;
       case "[":
         if(!data[p]) {
           for(let l = 1; l; l -= (code[c] === "]") - (code[c] === "[")) {
             if(c === code.length - 1) { throw new Error("Unmatched left brace"); }
-            ++c;
+            c++;
           }
         }
         break;
@@ -51,17 +51,21 @@ function brainfuck(code, input) {
         continue;
     }
 
-    if(++t === 0x1000000) {
+    if(t === 0xFFFFFF) {
       throw new Error("Ran for too many steps");
     }
+    t++;
   }
 
   return [output, t, portable];
 }
 
 // FIXME: Generate the input/output 100 times and make sure it works each time.
-function play(name, generator, ...solutions) {
+function play(name, instructions, generator, ...solutions) {
   console.log("\n## %s", name);
+  if(instructions) {
+    console.log("%s", instructions);
+  }
   if(generator === undefined) {
     return;
   }
@@ -160,6 +164,7 @@ console.log("# Human Brainfuck Machine");
 
 play(
   "Year 01: Mail Room",
+  "Copy the three bytes of input to the output.",
   () => {
     const x = shuffle(NUMBER + LETTER, 3).join("");
     return [x, x];
@@ -170,6 +175,7 @@ play(
 
 play(
   "Year 02: Busy Mail Room",
+  "Copy the input to the output.",
   () => {
     const x = shuffle(NUMBER + LETTER, random(8, 24)).join("");
     return [x, x];
@@ -179,6 +185,7 @@ play(
 
 play(
   "Year 03: Copy Floor",
+  "Output the alphabet.",
   () => ["", LETTER],
   "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.",
   "+++++++++++++[>++>+++++<<-]>[>.+<-]",
@@ -187,17 +194,20 @@ play(
 
 play(
   "Year 04: Scrambler Handler",
+  "Copy the input to the output, swapping pairs of bytes.",
   () => {
-    const x = shuffle(NUMBER + LETTER, random(4, 12) * 2).join("");
-    let y = "";
-    for(let i = 0; i < x.length; i++) { y += x[i ^ 1]; }
-    return [x, y];
+    const n = random(4, 12) * 2;
+    const x = shuffle(NUMBER + LETTER, n);
+    const y = new Array(n);
+    for(let i = 0; i < n; i++) { y[i] = x[i ^ 1]; }
+    return [x.join(""), y.join("")];
   },
   ",[>,.<.,]",
 );
 
 play(
   "Year 06: Rainy Summer",
+  "",
   () => {
     const n = random(4, 10);
     const ns = numbers(0, 9, n);
@@ -217,6 +227,7 @@ play(
 
 play(
   "Year 07: Zero Exterminator",
+  "Copy the input, except ASCII zeroes, to the output.",
   () => {
     const n = random(4, 12);
     const a = shuffle(NUMBER.slice(1) + LETTER, n).join("");
@@ -229,6 +240,7 @@ play(
 
 play(
   "Year 08: Tripler Room",
+  "For each input ASCII digit, triple it and output the result as two ASCII digts.",
   () => {
     const n = random(4, 10);
     const ns = numbers(0, 9, n);
@@ -244,6 +256,7 @@ play(
 
 play(
   "Year 09: Zero Preservation Initiative",
+  "Copy only ASCII zeroes from the input to the output.",
   () => {
     const n = random(4, 12);
     const a = shuffle(NUMBER.slice(1) + LETTER, n).join("");
@@ -256,6 +269,7 @@ play(
 
 play(
   "Year 10: Octoplier Suite",
+  "",
   () => { 
     const n = random(4, 8);
     const ns = numbers(0, 99, n);
@@ -271,10 +285,12 @@ play(
 
 play(
   "Year 11: Sub Hallway",
+  "",
 );
 
 play(
   "Year 12: Tetracontiplier",
+  "",
   () => {
     const n = random(4, 6);
     const ns = numbers(0, 255, n);
@@ -290,6 +306,7 @@ play(
 
 play(
   "Year 13: Equalization Room",
+  "",
   () => {
     const n = random(4, 6);
     const y = shuffle(NUMBER + LETTER, n).join("");
@@ -310,50 +327,62 @@ play(
 
 play(
   "Year 14: Maximation Room",
+  "",
 );
 
 play(
   "Year 16: Absolute Positivity",
+  "",
 );
 
 play(
   "Year 17: Exclusive Lounge",
+  "",
 );
 
 play(
   "Year 19: Countdown",
+  "",
 );
 
 play(
   "Year 20: Multiplication Workshop",
+  "",
 );
 
 play(
   "Year 21: Zero Terminated Sum",
+  "",
 );
 
 play(
   "Year 22: Fibonacci Visitor",
+  "",
 );
 
 play(
   "Year 23: The Littlest Number",
+  "",
 );
 
 play(
   "Year 24: Mod Module",
+  "",
 );
 
 play(
   "Year 25: Cumulative Countdown",
+  "",
 );
 
 play(
   "Year 26: Small Divide",
+  "",
 );
 
 play(
   "Year 28: Three Sort",
+  "",
   () => {
     const n = random(3, 8);
     let x = "";
@@ -370,14 +399,17 @@ play(
 
 play(
   "Year 29: Storage Floor",
+  "",
 );
 
 play(
   "Year 30: String Storage Floor",
+  "",
 );
 
 play(
   "Year 31: String Reverse",
+  "Output the bytes of the input in reverse order.",
   () => {
     const chars = shuffle(NUMBER + LETTER, random(8, 24));
     const x = chars.join("");
@@ -389,10 +421,12 @@ play(
 
 play(
   "Year 32: Inventory Report",
+  "",
 );
 
 play(
   "Year 34: Vowel Incinerator",
+  "",
   () => {
     const y = shuffle(NUMBER + CONSONANT, random(5, 10)).join("");
     return [merge(shuffle(VOWEL), y), y];
@@ -401,30 +435,37 @@ play(
 
 play(
   "Year 35: Duplicate Removal",
+  "",
 );
 
 play(
   "Year 36: Alphabetizer",
+  "",
 );
 
 play(
   "Year 37: Scavenger Chain",
+  "",
 );
 
 play(
   "Year 38: Digit Exploder",
+  "",
 );
 
 play(
   "Year 39: Re-Coordinator",
+  "",
 );
 
 play(
   "Year 40: Prime Factory",
+  "",
 );
 
 play(
   "Year 41: Sorting Floor",
+  "Output the input bytes in sorted order.",
   () => {
     const letters = shuffle(LETTER + LETTER, random(8, 24));
     const x = letters.join("");
