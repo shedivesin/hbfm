fdb93477881789a3d07b71e223fb1d6a57c9b174
shedivesin <>
2024-03-27 14:07:35 -0500

    be more agressive

--- bf.mjs
+++ bf.mjs
@@ -2,16 +2,8 @@
 import fs from "node:fs/promises";
 import {brainfuck} from "./lib/brainfuck.mjs";
 
-const input = process.argv[2]?.match(/\S+/g) ?? [];
-for(let i = 0; i < input.length; i++) {
-  const str = input[i];
-  const num = +str;
-  if(!Number.isInteger(num) || num <= 0) {
-    throw new Error("Invalid input " + str);
-  }
-
-  input[i] = num;
-}
+const input = process.argv[2]?.match(/[1-9][0-9]*/g) ?? [];
+for(let i = 0; i < input.length; i++) { input[i] = +input[i]; }
 
 const pathname = process.argv[3] ?? "/dev/stdin";
 let source = await fs.readFile(pathname, "ascii");
